4bb3d5635aa310a10f22676532f538661360ec40
Berke <stardusth2@gmail.com>
2017-09-29 16:02:10 +0300

    no message

--- The Cheesy Apocalypse/Assets/Scripts/AutoRotate.cs
+++ The Cheesy Apocalypse/Assets/Scripts/AutoRotate.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class AutoRotate : MonoBehaviour {
+public class AutoRotate : MonoBehaviour
+{
+    public Vector3 RotateAxis;
 
-	public int speed = 200;
-	
-	// Update is called once per frame
-	void Update () {
-		transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles + new Vector3 (0, 90, 0)), speed * Time.deltaTime);
-	}
+	void Update()
+    {
+        transform.Rotate(RotateAxis * Time.deltaTime);
+    }
 }
--- The Cheesy Apocalypse/Assets/Scripts/RigidFPC.cs
+++ The Cheesy Apocalypse/Assets/Scripts/RigidFPC.cs
@@ -4,89 +4,50 @@ using UnityEngine;
 
 public class RigidFPC : MonoBehaviour
 {
-	private Vector3 speed = new Vector3(6, 4, 5);
+	public Vector3 Speed = new Vector3(6, 4, 5);
 
-	Vector3 runSpeed = new Vector3 (6, 5, 5);
-	Vector3 normalSpeed = new Vector3 (4, 4, 3.5f);
-	Vector3 crouchSpeed = new Vector3 (2, 3, 2);
+	public float SidewaysMovementContoller = 1.1f;
+    public float StartingSmoothness = 1.2f;
+    public float BackwardsSlower = 1.7f;
 
-	Transform cam;
-	bool grounded;
+    bool Grounded;
 
-	float sidewaysMovementSlower = 1.1f;// how slow should sideway movement be? higher -> slower
-	float movementStartSlowAmount = 1.2f;//how slowly should we start to move? higher -> slower
-	float backwardsSlower = 1.7f;// how slow should backwards movement be? higher -> slower
-	float cameraJiggleAmount = 3f;//how fast shoul it jiggle according to speed
-	float fallDamageMultipler = 20;//how much damage should we deal? multiplied by velocity
-	float fallDamgeMinH = 8;//minimun velocity to receive fall damage
-
-	void FixedUpdate () {
-
-		//run and "crouch" change speed values
-		if(Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)){
-			speed = crouchSpeed;
-
-		}else if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)){
-			speed = runSpeed;
-
-		}else{
-			speed = normalSpeed;
-
-		}
-
-
-		//---movement---
-
-		//Get input and multiply them with speed values
-		Vector3 Keyinput = new Vector3(Input.GetAxis("Horizontal") * speed.z, GetComponent<Rigidbody>().velocity.y, Input.GetAxis("Vertical") * speed.x);
-
-		//while we are touching ground
-		if(grounded == true){
-
-			//if we are moving in two directions make it slower to stop being able to diagonaly move faster
-			if(Keyinput.x != 0 && Keyinput.z != 0)
-            {
-				Keyinput.x /= sidewaysMovementSlower;
-				Keyinput.z /= sidewaysMovementSlower;			
-			}
-			//slower backwards
-			if(Keyinput.z < 0) Keyinput.z /= backwardsSlower;
-
-			Vector3 locVel = transform.TransformDirection(Keyinput); //transform local directions to world directions
-			locVel = locVel - GetComponent<Rigidbody>().velocity; //umm
-			locVel /= movementStartSlowAmount; //make it start slowly
-			//add our velocity
-			GetComponent<Rigidbody>().velocity += locVel;
-		}
-
-		//jump
-		if(Input.GetKeyDown(KeyCode.Space) && grounded && GetComponent<Rigidbody>().velocity.y < speed.y - 1)
+    void FixedUpdate()
+    {
+		if(Grounded)
         {
-			GetComponent<Rigidbody>().velocity += new Vector3 (0, speed.y, 0);
-		}
+            Vector3 Keyinput = new Vector3(Input.GetAxis("Horizontal") * Speed.z, GetComponent<Rigidbody>().velocity.y, Input.GetAxis("Vertical") * Speed.x);
 
+            if (Keyinput.x != 0 && Keyinput.z != 0)
+            {
+				Keyinput.x /= SidewaysMovementContoller;
+				Keyinput.z /= SidewaysMovementContoller;			
+			}
+
+            if(Keyinput.z < 0)
+            {
+                Keyinput.z /= BackwardsSlower;
+            }
+
+			Vector3 LocalVelocity = transform.TransformDirection(Keyinput);
+			LocalVelocity = LocalVelocity - GetComponent<Rigidbody>().velocity;
+			LocalVelocity /= StartingSmoothness;
+			GetComponent<Rigidbody>().velocity += LocalVelocity;
+
+            if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Rigidbody>().velocity.y < Speed.y - 1)
+            {
+                GetComponent<Rigidbody>().velocity += new Vector3(0, Speed.y, 0);
+            }
+        }
 	}
 
-
-
-	void OnTriggerEnter (){
-		//fall damage
-		//check velocity
-		/*if(GetComponent<Rigidbody>().velocity.y > fallDamgeMinH){
-			//damage more according to the velocity
-			//health.Damage(rigidbody.velocity.y * fallDamageMultipler);
-
-		}*/
-
+	void OnTriggerStay()
+    {
+		Grounded = true;
 	}
 
-	//are we touching ground?
-	void OnTriggerStay () {
-		grounded = true;
-	}
-
-	void OnTriggerExit () {
-		//we have jumped or falled
-		grounded = false;
+	void OnTriggerExit()
+    {
+		Grounded = false;
 	}
 }
