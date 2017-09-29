ebbc87ba6b6cddf41d9a6f50cdaed33f13a25a11
Atahan <atahan012000@gmail.com>
2017-09-29 15:12:53 +0300

    no message

---
+++ The Cheesy Apocalypse/Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+
+	public int speed = 200;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles + new Vector3 (0, 90, 0)), speed * Time.deltaTime);
+	}
+}
