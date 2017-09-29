7f9a5a4b8a9cd829383817ea59766f97070cc7a9
Berke <stardusth2@gmail.com>
2017-09-29 14:34:42 +0300

    no message

--- The Cheesy Apocalypse/Assets/Scripts/PlayerController.cs
+++ The Cheesy Apocalypse/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 moveDirection = Vector3.zero;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 }
