97274f179ea68cfea5afaa019b18c164af8992c2
Atahan <atahan012000@gmail.com>
2017-09-30 11:34:01 +0300

    no message

---
+++ The Cheesy Apocalypse/Assets/Editor/GridSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridSnap : MonoBehaviour {
+	#if UNITY_EDITOR
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	#endif
+}
