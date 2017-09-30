1ce14dccba1e8d9cb9a421b965b06247f5f16fd3
Atahan <atahan012000@gmail.com>
2017-09-30 11:26:06 +0300

    no message

--- The Cheesy Apocalypse/Assets/Scripts/AISpawner.cs
+++ The Cheesy Apocalypse/Assets/Scripts/AISpawner.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class AISpawner : MonoBehaviour {
 
+	public GameObject ai;
+	Transform[] spoints;
+
 	// Use this for initialization
 	void Start () {
-		
+		spoints = GetComponentsInChildren<Transform> ();
+		Invoke ("SpawnEnemy", 1f);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void SpawnEnemy (){
+		Transform rndPos = GetRandomSpawnPoint ();
+		Instantiate (ai, rndPos.position, rndPos.rotation);
+		Invoke ("SpawnEnemy", Random.Range(5f, 10f));
+	}
+
+	Transform GetRandomSpawnPoint (){
+		return spoints [Random.Range (0, spoints.Length)];
 	}
 }
