87bb42601a224fd0894b5e7cea3a88352a76b5ed
Atahan <atahan012000@gmail.com>
2017-09-30 20:18:39 +0300

    no message

---
+++ The Cheesy Apocalypse/Assets/Scripts/GridSnap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GridSnap : MonoBehaviour {
+	#if UNITY_EDITOR
+
+	public enum snapType {normal, floor, obj, wall};
+	public snapType mySnapType = snapType.normal;
+
+	void Update () {
+		if (!Application.isPlaying) {
+			float posSnap = 1f;
+			float rotSnap = 45f;
+			switch(mySnapType){
+			case snapType.normal:
+				posSnap = 1f;
+				rotSnap = 90f;
+				break;
+			case snapType.floor:
+				posSnap = 10f;
+				rotSnap = 45f;
+				break;
+			case snapType.obj:
+				posSnap = 2f;
+				rotSnap = 45f;
+				break;
+			case snapType.wall:
+				posSnap = 10f;
+				rotSnap = 90f;
+				break;
+			}
+
+			transform.position = new Vector3 (((int)(transform.position.x / posSnap)) * posSnap, 0, ((int)(transform.position.z / posSnap)) * posSnap);
+			//transform.rotation = Quaternion.Euler (((int)(transform.rotation.eulerAngles.x / rotSnap)) * rotSnap, 0, ((int)(transform.rotation.eulerAngles.z / rotSnap)) * rotSnap);
+
+		}
+	}
+	#endif
+}
