62800d052ae32c592201107ba569992ecef14679
Atahan <atahan012000@gmail.com>
2017-10-01 20:24:06 +0300

    no message

---
+++ The Cheesy Apocalypse/Assets/LevelEndVisuals.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndVisuals : MonoBehaviour {
+
+	public static LevelEndVisuals s;
+
+	public GameObject winScreen;
+	public GameObject loseScreen;
+
+	// Use this for initialization
+	void Start () {
+		s = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool isLocked = false;
+
+	public void Win (){
+		if (isLocked)
+			return;
+		isLocked = true;
+
+		GameObject.FindGameObjectWithTag ("Player").GetComponent<RigidFPC> ().enabled = false;
+		Time.timeScale = 0;
+
+		winScreen.SetActive (true);
+		loseScreen.SetActive (false);
+
+		PlayerPrefs.SetInt (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex.ToString (), 1);
+	}
+
+	public void Lose (){
+		if (isLocked)
+			return;
+		isLocked = true;
+
+		GameObject.FindGameObjectWithTag ("Player").GetComponent<RigidFPC> ().enabled = false;
+		Time.timeScale = 0;
+
+		winScreen.SetActive (false);
+		loseScreen.SetActive (true);
+	}
+
+	public void BackToMenu (){
+		LevelSelector.s.SelectLevel (0);
+	}
+}
---
+++ The Cheesy Apocalypse/Assets/Pauser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pauser : MonoBehaviour {
+
+	public GameObject pScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	bool isPause = false;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
+			if (!isPause) {
+				Time.timeScale = 0;
+				isPause = true;
+				pScreen.SetActive (true);
+			} else {
+				Time.timeScale = 1;
+				isPause = false;
+				pScreen.SetActive (false);
+			}
+		}
+	}
+
+	public void UnPause (){
+		Time.timeScale = 1;
+		isPause = false;
+		pScreen.SetActive (false);
+	}
+
+	public void BackToMenu (){
+		LevelSelector.s.SelectLevel (0);
+	}
+}
