890f2cd6c0b1575584e017901ef11f4296da8185
Atahan <atahan012000@gmail.com>
2017-10-01 21:09:43 +0300

    no message

---
+++ The Cheesy Apocalypse/Assets/BeginningAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginningAnim : MonoBehaviour {
+
+	public GameObject[] screens;
+	int curOne = 0;
+
+	// Use this for initialization
+	void Start () {
+		ActivateScreen (curOne);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ActivateScreen(int number){
+		foreach (GameObject sc in screens) {
+			sc.SetActive (false);
+		}
+
+		screens [number].SetActive (true);
+	}
+
+	public void Next (){
+		curOne++;
+		if (curOne < screens.Length)
+			ActivateScreen (curOne);
+		else
+			this.gameObject.SetActive (false);
+	}
+}
