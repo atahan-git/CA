c16ab1b7e0ad1cb79be8c3f94c76f2e515a3bf5b
Atahan <atahan012000@gmail.com>
2017-10-01 15:02:44 +0300

    no message

--- The Cheesy Apocalypse/Assets/Scripts/DecalSpawner.cs
+++ The Cheesy Apocalypse/Assets/Scripts/DecalSpawner.cs
@@ -16,7 +16,9 @@ public class DecalSpawner : MonoBehaviour {
 		int am = (int)Random.Range (amount.x, amount.y);
 
 		for (int i = 0; i <= am; i++) {
-			Instantiate(toSpawn[Random.Range(0,toSpawn.Length)],new Vector3(transform.position.x + Random.Range(-area.x/2f,area.x/2f), transform.position.y, transform.position.z + Random.Range(-area.z/2f,area.z/2f)), Quaternion.identity);
+			Instantiate(toSpawn[Random.Range(0,toSpawn.Length)],
+				new Vector3(transform.position.x + Random.Range(-area.x/2f,area.x/2f), transform.position.y, transform.position.z + Random.Range(-area.z/2f,area.z/2f)),
+				Quaternion.Euler(0,Random.Range(0,360),0));
 		}
 	}
 	
